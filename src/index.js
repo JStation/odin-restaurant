ef47f3866fe46274790b75ce1c98dc666a0da284
JStation <jstation@grim.link>
2022-08-17 13:30:53 -0700

    setup HTMLwebpackplugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log("index.js processed and loaded");
+
+// Overview
+// Load Page w/ static components (header, nav, empty container div, footer)
+// handle click events on nav links to trigger page modules (info, menu, contact)
+// load info module by default

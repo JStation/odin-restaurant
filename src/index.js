13c3b8781a314b60f70d1ebffd3e6267d2ff8b97
JStation <jstation@grim.link>
2022-08-18 10:33:54 -0700

    create setup.js and setupSite scripts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 console.log("index.js processed and loaded");
 
 import css from "./style.css";
+import setupSite from "./setup.js";
 
 // Overview
 // Load Page w/ static components (header, nav, empty container div, footer)
 // handle click events on nav links to trigger page modules (info, menu, contact)
 // load info module by default
+
+setupSite();
